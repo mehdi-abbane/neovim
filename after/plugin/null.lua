0b76c6eb40a118aff06c64477835ef89d79ec081
Mehdi.Ab <abbanemehdi2006@gmail.com>
2025-04-10 05:48:24 +0100

    M

--- after/plugin/null.lua
+++ after/plugin/null.lua
@@ -5,20 +5,15 @@ null_ls.setup({
 	sources = {
 		-- Python (black)
 		null_ls.builtins.formatting.black.with({ extra_args = { "--fast" } }),
-
 		-- JavaScript/TypeScript (prettier)
 		null_ls.builtins.formatting.prettier,
-
 		-- C++ (clang-format)
 		null_ls.builtins.formatting.clang_format,
-
 		-- CSS (prettier)
 		null_ls.builtins.formatting.rustfmt,
 		--PHP
 		null_ls.builtins.formatting.phpcsfixer
-
 	},
-
 })
 
 -- Format on save
